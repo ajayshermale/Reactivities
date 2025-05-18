a2f1419235eb6ebf291027c9480fbd460d001e98
AjayShermaledbugged <Ajay.shermale@dbuggedprogrammers.com>
2025-05-18 23:31:14 +0530

    React May182025
    Client react
    - Vite
    - React project files
    - React Dev tool
    - Typescript
    - Axios - access data instead of javascript
    - Material-UI

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+.WithOrigins("http://localhost:3000","https://localhost:3000"));
+
 app.MapControllers();
 
 
